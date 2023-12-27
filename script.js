7f808d216a17c89b8156e8e122faf43b9ad66b22
Abirami P <146667433+AbiramiVP@users.noreply.github.com>
2023-12-27 17:31:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+
+setTimeout(()=>{
+    document.body.innerHTML = "10";
+    setTimeout(()=>{
+        document.body.innerHTML = "9";
+        setTimeout(()=>{
+            document.body.innerHTML = "8";
+            setTimeout(()=>{
+                document.body.innerHTML = "7";
+                setTimeout(()=>{
+                    document.body.innerHTML = "6";
+                    setTimeout(()=>{
+                        document.body.innerHTML = "5";
+                        setTimeout(()=>{
+                            document.body.innerHTML = "4";
+                            setTimeout(()=>{
+                                document.body.innerHTML = "3";
+                                setTimeout(()=>{
+                                    document.body.innerHTML = "2";
+                                    setTimeout(()=>{
+                                        document.body.innerHTML = "1";
+                                        setTimeout(()=>{
+                                            document.body.innerHTML =
+                                            ("<h3>Happy Independence Day!</h3>")
+                                            
+                                        }, "1000");
+                                    }, "1000");
+                                }, "1000"); 
+                            }, "1000");
+                        }, "1000"); 
+                    }, "1000");  
+                }, "1000");  
+            }, "1000");  
+        }, "1000");
+    }, "1000");
+},"1000");
